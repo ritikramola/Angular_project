cc6d2f8d73aa32a8559e759b5d2a6174ed1a9187
ritik.ramola <ritik.ramola@globallogic.com>
2025-06-28 12:49:20 +0530

    api hit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http'; // Importing HttpClient for making HTTP requests
+import { provideHttpClient, withFetch } from '@angular/common/http'; // Importing HttpClient for making HTTP requests
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideHttpClient(withFetch()),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
--- src/app/component/roles/roles.component.ts
+++ src/app/component/roles/roles.component.ts
@@ -1,36 +1,137 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';  // Importing necessary Angular core module
+// Importing OnInit lifecycle hook to perform initialization logic
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../model/interface/role';
+
 
 //component decorator //means what this class is going to do
 //selector is the name of the component
 @Component({
   selector: 'app-roles',
   imports: [FormsModule],  // any imports needed for this component
-  standalone: true, // this component is standalone
+  standalone: true,         // this component is standalone, meaning it can be used independently without being part of a module
   // templateUrl is the path to the HTML file for this component
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-// class is the logic of the component //we create variable and functions here
-export class RolesComponent {
+
+
+// class is the logic of the component //we create variable and functions here //implementing mean using it as lifecycle hook otherwise it's an function only
+export class RolesComponent implements OnInit {
+
+//constructor(private http: HttpClient) { }  // Injecting HttpClient to make HTTP requests //this is the old way
+
+  rolelist: IRole[] = [] // variable to hold the roles
+  http = inject(HttpClient); // Injecting HttpClient to make HTTP requests using the new inject function //this is the new way
+
+
+ngOnInit(): void {
+  this.getAllRoles(); // Calling the method to get all roles when the component is initialized
+  // This method is called when the component is initialized
+  // You can perform any initialization logic here
+  //   alert("Welcome")    // this will run as soon as the component is initialized
+}
+
+getAllRoles() {
+  this.http.get('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles').subscribe((res:any)=>{
+    this.rolelist = res.data; // Assigning the response data to the rolelist variable
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // // variable to hold the roles
 
-  firstname: string = 'Tutorial';
-  angularversion: string = 'Angular 17';
-  version: number = 17;
-  isActive: boolean = true;
-  currentDate: Date = new Date();
+//   firstname: string = 'Tutorial';
+//   angularversion: string = 'Angular 17';
+//   version: number = 17;
+//   isActive: boolean = true;
+//   currentDate: Date = new Date();
 
-  inputType: string = "button"; // this is a variable that holds the type of the input element
-  selectState : string = ''; // this is a variable that holds the selected state
-  //we can only use the variable in the HTML file i.e // roles.component.html
+//   inputType: string = "button"; // this is a variable that holds the type of the input element
+//   selectState : string = ''; // this is a variable that holds the selected state
+//   //we can only use the variable in the HTML file i.e // roles.component.html
 
-  showWelcomeAlert() {  // this is a function that shows an alert when called
-    alert('Welcome to the Angular 17 Tutorial');
-  }
+//   showWelcomeAlert() {  // this is a function that shows an alert when called
+//     alert('Welcome to the Angular 17 Tutorial');
+//   }
 
-  showMessage(message:String) { // this is a function that shows an alert when called
-    alert(message);
-  }
+//   showMessage(message:String) { // this is a function that shows an alert when called
+//     alert(message);
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
 }
  
